73c13a075694920e63ff691c1f6eca09bcbb95cd
Ini Oguntola <ioguntol@gmail.com>
2021-12-20 17:44:09 +0000

    Update RedisBridge/bridge.py

--- RedisBridge/bridge.py
+++ RedisBridge/bridge.py
@@ -37,7 +37,7 @@ class RedisBridge:
     """
 
     def __init__(self, name=None, host='localhost', port=6379, db=0):
-        self.connection = redis.Redis(host='localhost', port=6379, db=0, health_check_interval=1)
+        self.connection = redis.Redis(host=host, port=port, db=db, health_check_interval=1)
         self.pubsub = self.connection.pubsub(ignore_subscribe_messages=True)
         self.thread = None
 
