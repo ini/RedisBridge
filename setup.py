e1c39f5f46b5125af8fdb8ab4454fb6884602ead
Ini <ioguntol@gmail.com>
2022-01-01 06:06:00 -0500

    Fix setup.py bug

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
 	long_description=long_description,
 	long_description_content_type='text/markdown',
 	url='https://gitlab.com/cmu_asist/RedisBridge',
-	packages=setuptools.find_packages(include=['RedisBridge']),
+	packages=setuptools.find_packages(),
 	classifiers=[
 		'Programming Language :: Python :: 3',
 		'License :: OSI Approved :: MIT License',
