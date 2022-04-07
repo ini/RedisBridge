e17315b3653b6f1643907964f733b5ee09be7062
Ini <ioguntol@gmail.com>
2022-04-07 19:28:06 -0400

    Fix client output buffer limit bug

--- RedisBridge/bridge.py
+++ RedisBridge/bridge.py
@@ -320,11 +320,8 @@ class RedisBridge(Loggable):
             self._connection.ping()
 
             # Set client pubsub hard / soft output buffer limits
-            # 1 GB hard limit, 256 MB per 60 seconds soft limit
-            self._connection.config_set(
-                'client-output-buffer-limit',
-                f'normal 0 0 0 slave 268435456 67108864 60 pubsub 1GB 256MB 60',
-            )
+            # 1 GB hard limit, 64 MB per 60 seconds soft limit
+            self._connection.config_set('client-output-buffer-limit', f'pubsub {2**30} {2**26} 60')
 
             self.logger.info(f"{self}:  Connected to Redis at {host}:{port}, database {db}")
             self._pubsub = self._connection.pubsub(ignore_subscribe_messages=True)
