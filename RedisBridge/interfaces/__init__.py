e3cfc5d785c9638495101d07a3fbb74620cd0d45
Ini <ioguntol@gmail.com>
2022-04-07 22:00:29 -0400

    Add deprecation warning for CallbackDecorator

--- RedisBridge/interfaces/__init__.py
+++ RedisBridge/interfaces/__init__.py
@@ -1,2 +1,12 @@
 from .base import RedisInterface
 from .callback import CallbackInterface
+
+
+class CallbackDecorator(CallbackInterface):
+
+	def __init__(self, *args, **kwargs):
+		self.logger.warning(
+			DeprecationWarning("'CallbackDecorator' is being deprecated. Use 'CallbackInterface' instead."),
+			stack_info=True,
+		)
+		super().__init__(*args, **kwargs)
